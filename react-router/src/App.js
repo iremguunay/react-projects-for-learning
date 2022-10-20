a5d38f6dde93689935e7ca4e81131d19316aee1b
Irem Gunay <iremguunay@gmail.com>
2022-10-21 00:16:38 +0300

    Usage of React Router v6 with sample structure

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/users">
+              Users
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="main-body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ react-router/src/components/Error404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error404() {
+  return (
+    <>
+      <h2>Error404</h2>
+      <h3>This page could not found!</h3>
+    </>
+  );
+}
+
+export default Error404;
